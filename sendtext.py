be79d463c77ae745d2596365a347352f4e24a33e
Kriszcson <faskertikr@gmail.com>
2020-07-20 08:01:17 +0100

    Basic structure done. Sendtext module ready for use. Useful code module might have uses in the future.

--- sendtext.py
+++ sendtext.py
@@ -8,3 +8,6 @@ def telegram_bot_sendtext(bot_message):
     send_text = 'https://api.telegram.org/bot' + bot_token + \
         '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
+
+
+telegram_bot_sendtext("I'm sorry Dave")
