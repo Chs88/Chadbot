7eb37a4aa5793e3d774bfb2b56fd676a48317c01
Kriszcson <faskertikr@gmail.com>
2020-07-21 13:25:07 +0100

    Finished the working bot version 1.0

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import date
+from decouple import config
 
 
 class Chadbot:
     def __init__(self):
         self.name = "Chadbot"
-        self.url = 'https://seths.blog/2018/09/the-solution-to-stalled/'
+        self.url = self.get_random_url()
+        self.today = date.today()
+        self.DotW = self.day_of_the_week()  # Day of the week
 
     def sendtext(self, bot_message):
-        bot_token = '1340709229:AAHEPXRY5yRQmCP_XPcVoyJ05lus0P-Ia7o'
-        bot_chatID = '1249893195'
+        bot_token = config('BOT_TOKEN')
+        bot_chatID = config('CHAT_ID')
         send_text = 'https://api.telegram.org/bot' + bot_token + \
             '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
         response = requests.get(send_text)
@@ -27,9 +31,36 @@ class Chadbot:
         blog_post = []
         for paragraph in paragraphs:
             blog_post.append(paragraph.text)
-        blog_post.insert(0, "Title: " + title.text)
+        blog_post.insert(0, title.text.upper())
         return blog_post
+
+    # Uses a starting url to exploit the pages Random post button that always gives a random post. Bad practice but works
+    def get_random_url(self):
+        starting_page = requests.get(
+            "https://seths.blog/2020/07/fake-deadlines/")
+        soup = BeautifulSoup(starting_page.content, 'html.parser')
+
+        ## Snajdipapa ##
+        random_url = soup.find(
+            'div', class_='random-post').find('form').get('action')
+        return random_url
+
+    def run(self):
+        bot.sendtext("================================")
+        bot.sendtext(
+            f"Good morning Krisz, today is: {self.DotW}. {self.today}!")
+        bot.sendtext("Here is a Blog post:")
+        post = self.get_blog_post(self.url)
+        bot.send_blogpost(post)
+        bot.sendtext("Have a good day!")
+        bot.sendtext("================================")
+
+    def day_of_the_week(self):
+        days = ["Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"]
+        dayNumber = date.weekday(self.today)
+        return days[dayNumber]
 
 
 bot = Chadbot()
-bot.send_blogpost(bot.get_blog_post(bot.url))
+bot.run()
